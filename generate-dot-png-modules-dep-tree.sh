4ce7b1bee538e6360e099a39395a8d6c16ff5ce9
Rostislav Svoboda <rsvoboda@redhat.com>
2016-06-11 22:20:52 +0200

    generate-dot-png-modules-dep-tree.sh

---
+++ generate-dot-png-modules-dep-tree.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# Searches for module.xml and generates .dot files with module dependencies
+# PNG files are generated using dot command from Graphviz  
+# You can generate use custom sub-directory of root modules directory to limit all the generated dependencies
+# 
+# Rostislav Svoboda rsvoboda@redhat.com
+#
+
+if [ $# -ne 1 ]; then
+  echo "I require one argument, usage: $0 JBOSS_MODULES_DIRECTORY"; exit 1
+fi
+
+type dot >/dev/null 2>&1 || { echo >&2 "I require dot from Graphviz but it's not installed.  Aborting."; exit 1; }
+
+JBOSS_MODULES_DIRECTORY=$1
+
+echo "digraph {" > all.dot
+
+for MODULE_XML_PATH in `find $JBOSS_MODULES_DIRECTORY | grep module.xml`; do
+
+   echo "Processing $MODULE_XML_PATH"
+
+   NAMESPACE=`grep "xmlns=" $MODULE_XML_PATH | sed "s/.*xmlns=\"\(.*1\..\).*/\1/"`  ## e.g. urn:jboss:module:1.3
+   MODULE_NAME=$(echo -e "setns p=$NAMESPACE\nxpath /p:module/@name" | xmllint --shell $MODULE_XML_PATH | grep "=" | cut -f 2 -d "=")
+   DEPENDENCIES=$(echo -e "setns p=$NAMESPACE\nxpath /p:module/p:dependencies/p:module/@name" | xmllint --shell $MODULE_XML_PATH | grep "=" | cut -f 2 -d "=")
+
+   if [ -z "$DEPENDENCIES" ]; then
+      echo "   Skipping as dependencies are empty"
+   else
+      echo "digraph {" > $MODULE_NAME.dot
+      for i in $DEPENDENCIES; do 
+         echo "\"$MODULE_NAME\" -> \"$i\"" | tee -a all.dot >> $MODULE_NAME.dot
+      done
+      echo "}" >> $MODULE_NAME.dot
+
+      dot -Tpng $MODULE_NAME.dot > $MODULE_NAME.png
+   fi
+done
+
+echo "}" >> all.dot
+echo "Generating PNG for all dependencies, it may take awhile."
+dot -Tpng all.dot > all.png
+
